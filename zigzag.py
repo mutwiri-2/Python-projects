21886cd1eea83d40a8976effebc5997c7298fb8b
mutwiri-2 <devmutwiri2@gmail.com>
2020-04-17 22:21:36 +0300

    version 1.0

---
+++ zigzag.py
@@ -0,0 +1,23 @@
+# a simple animation program that creates a back and forth zigzag pattern until user stops it by pressing ctrl+c
+
+import sys, time
+
+indent = 0 # how many white spaces to add
+indent_increasing = True # if white spaces are increasing or not
+
+try:
+    while True: # main program loop
+        print(' ' * indent, end='')
+        print('******')
+        time.sleep(0.1)
+    
+        if indent_increasing:
+            indent += 1
+            if indent == 20:
+                indent_increasing = False
+        else:
+            indent -= 1
+            if indent == 0:
+                indent_increasing = True
+except KeyboardInterrupt:
+    sys.exit()
