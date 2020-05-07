96d0b56061744b8d72771e5d1a8fd1b87f3dfd2f
mutwiri-2 <devmutwiri2@gmail.com>
2020-05-07 13:37:09 +0300

    number guessing game

---
+++ number_guessing_game/number_guessing_game.py
@@ -0,0 +1,21 @@
+import random
+
+lower_bound = 1
+upper_bound = 20
+secret_number = random.randint(lower_bound, upper_bound)
+chances = 5
+
+print("I am thinking of a number between {} and {}...".format(lower_bound, upper_bound))
+for number_of_guess in range(1, chances+1):
+    print("Number of guesses remaining: {}".format(chances+1 - number_of_guess))
+    guess = int(input("Guess my number: "))
+    if guess > secret_number and number_of_guess != chances:
+        print("Try a lower value")
+    elif guess < secret_number and number_of_guess != chances:
+        print("Try a higher value")
+    else:
+        break
+if guess == secret_number:
+    print("You win!! You got it right in {} guesses".format(number_of_guess))
+else:
+    print("You loose!! My number was {}".format(secret_number))
