31aa1b862161a5a4264a5d4435e54af54cbcaf98
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-01 18:41:57 +0300

    for loop will iterate exactly chances n.o of times

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -8,13 +8,14 @@ chances = 5
 print("I am thinking of a number between {} and {}...".format(lower_bound, upper_bound))
 for number_of_guess in range(1, chances+1):
     print("Number of guesses remaining: {}".format(chances+1 - number_of_guess))
-    guess = int(input("Guess my number: "))
-    if guess > secret_number and number_of_guess != chances:
+    guess = int(input("Type your guess and press enter: "))
+    if guess > secret_number:
         print("Try a lower value")
-    elif guess < secret_number and number_of_guess != chances:
+    elif guess < secret_number:
         print("Try a higher value")
     else:
         break
+
 if guess == secret_number:
     print("You win!! You got it right in {} guesses".format(number_of_guess))
 else:
