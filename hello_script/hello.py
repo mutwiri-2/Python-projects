910a98ba7a95d42c6b5740facacc8457e171b8ae
mutwiri-2 <devmutwiri2@gmail.com>
2020-07-07 00:44:50 +0300

    hello world

--- hello_script/hello.py
+++ hello_script/hello.py
@@ -1,3 +1,6 @@
 #! python3
-# hello.py - Prints hello world. A program to keep the tradition alive. It is often the first program written by people learning to code
+# hello.py - Prints hello world.
+# A program to keep the tradition alive. It is often the first program written
+# by people learning to code
+
 print("Hello World!")
